087bd5adaf829f4c4667b7c34c8b11a63c24999f
Egor <egoralvolk@gmail.com>
2018-02-25 13:51:03 +0700

    Commit 13. Changed mergesort.

--- cpp/merge_sort.cpp
+++ cpp/merge_sort.cpp
@@ -4,73 +4,68 @@
 using namespace std;
 
 template <typename T>
-T swapElements(T &a, T &b)
-{
+T swapElements(T &a, T &b) {
   T tmp = a;
   a = b;
   b = tmp;
 }
 
 template <typename T>
-void printArray(int size, T array[])
-{
-  for(int i = 0; i < size; i++)
+void printArray(int size, T array[]) {
+  for(int i = 0; i < size; i++) {
     cout << array[i] << ' ';
+  }
   cout << endl;
-  return;
 }
 
 template <typename T>
-void merge(int left, int mid, int right, T* array)
-{
-  int i = left,
-      j = mid + 1,
-      k = 0,
-      size = right - left + 1;
+void merge(int left, int mid, int right, T* array) {
+  int i = left;
+  int j = mid + 1;
+  int k = 0;
+  int size = right - left + 1;
 
-  T* arr = new T[size];
+  auto * arr = new T[size];
 
-  while ((i < mid + 1) && (j < right + 1))
+  while ((i <= mid) && (j <= right)) {
     arr[k++] = (array[i] < array[j]) ? array[i++] : array[j++];
+  }
 
-  while (i < mid + 1)
-  {
+  while (i <= mid) {
     arr[k] = array[i++];
     k++;
   }
 
-  while (j < right + 1)
-  {
+  while (j <= right) {
     arr[k] = array[j++];
     k++;
   }
 
-  for(int z = 0; z < size; z++)
+  for(int z = 0; z < size; z++) {
     array[z + left] = arr[z];
+  }
 }
 
 template <typename T>
-void mergeSortRecursive(int left, int right, T* array)
-{
-  if (left < right)
-  {
-    int mid = (left + right) >> 1;
-    mergeSortRecursive(left, mid, array);
-    mergeSortRecursive(mid+1, right, array);
+void mergeSortRecursive(int left, int right, T* array) {
+  if (left < right) {
+    int mid = (left + right) / 2;
+    mergeSortRecursive(left, mid , array);
+    mergeSortRecursive(mid + 1, right, array);
     return merge(left, mid, right, array);
   }
 }
 
 template <typename T>
-void mergeSort(int size, T* array)
-{
-  for(int i = 1; i < size; i *= 2)
-    for(int j = 0; j < size - i; j += 2 * i)
-     merge(j, j + i - 1, min(j + i*2, size) - 1, array);
+void mergeSort(int size, T* array) {
+  for(int i = 1; i < size; i *= 2) {
+    for(int j = 0; j < size - i; j += 2 * i) {
+      merge(j, j + i - 1, min(j + i*2, size) - 1, array);
+    }
+  }
 }
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
   int size = 10;
 
   int array_int[] = {4, 5, 6, 7, 1, 3, 2, 9, 8, 0};
@@ -89,15 +84,15 @@ int main(int argc, char * argv[])
   cout << "----------" << endl;
 
   cout << "Sorted array of integers: ";
-  mergeSortRecursive(0, size-1, array_int);
+  mergeSortRecursive(0, size - 1, array_int);
   printArray(size, array_int);
 
   cout << "Sorted array of floats: ";
-  mergeSortRecursive(0, size-1, array_float);
+  mergeSortRecursive(0, size - 1, array_float);
   printArray(size, array_float);
 
   cout << "Sorted array of doubles: ";
-  mergeSortRecursive(0, size-1, array_double);
+  mergeSortRecursive(0, size - 1, array_double);
   printArray(size, array_double);
 
   return 0;
